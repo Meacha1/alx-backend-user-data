fd7cfad05503c682bf6f6b1c464fc275b7e9a636
Meacha1 <meachattd@gmail.com>
2023-08-03 20:13:32 +0300

    regex

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 '''regex to obfuscate sensitive information'''
 
+import logging
 import re
 from typing import List
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
     """
     Obfuscates sensitive fields in the log message.
 
@@ -12,10 +15,32 @@ def filter_datum(fields: List[str], redaction: str, message: str, separator: str
     fields: a list of strings representing all fields to obfuscate
     redaction: a string representing by what the field will be obfuscated
     message: a string representing the log line
-    separator: a string representing by which character is separating all fields in the log line (message)
+    separator: a string representing by which character is separating
+    all fields in the log line (message)
 
     Returns:
     The log message with sensitive fields obfuscated.
     """
-    regex_pattern = '|'.join(f'({field}=.*?)({separator}|$)' for field in fields)
+    regex_pattern = '|'.join(f'({field}=.*?)({separator}|$)'
+                             for field in fields)
     return re.sub(regex_pattern, f'{redaction}\\2', message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """ Redacting Formatter class"""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ Redacting Formatter class"""
+        record.msg = filter_datum(self.fields,
+                                  self.REDACTION, record.msg, self.SEPARATOR)
+        return super(RedactingFormatter, self).format(record)
