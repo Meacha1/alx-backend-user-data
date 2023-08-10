8b1e7cbb80f8047b47533094e83222214e09e9ef
Meacha1 <meachattd@gmail.com>
2023-08-10 12:34:17 +0300

    Basic auth

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -8,6 +8,9 @@ from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
 from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
+
+
 
 
 app = Flask(__name__)
@@ -19,6 +22,8 @@ auth_type = getenv("AUTH_TYPE")
 
 if auth_type == "auth":
     auth = Auth()
+elif auth_type == "basic_auth":
+    auth = BasicAuth()
 
 
 @app.errorhandler(404)
---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+'''Basic authentication module'''
+import base64
+import binascii
+import re
+from typing import Tuple, TypeVar
+from .auth import Auth
+
+
+class BasicAuth(Auth):
+    '''Basic authentication class'''
+    def __init__(self, *args: str, **kwargs: str) -> None:
+        '''Constructor'''
+        super().__init__(*args, **kwargs)
+
+    def  _decode(self, encoded_string: str) -> Tuple[str, str]:
+        '''Decodes a base64 string'''
+        try:
+            decoded_string = base64.b64decode(encoded_string).decode('utf-8')
+        except (binascii.Error, UnicodeDecodeError):
+            return None, None
+        return re.split(':', decoded_string, 1)
+
+    def  _validate(self, user: str, password: str) -> bool:
+        '''Validates a user and password'''
+        return user == self.user and password == self.password
+
+    def  _authenticate(self, header: str) -> bool:
+        '''Authenticates a request'''
+        if not header:
+            return False
+        if not header.startswith('Basic '):
+            return False
+        user, password = self._decode(header[6:])
+        if not user or not password:
+            return False
