88e802e921e0cc04a123f566b2be758d08a8a5a7
Meacha1 <meachattd@gmail.com>
2023-08-10 12:24:32 +0300

    Request validation

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -8,7 +8,6 @@ from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
 from api.v1.auth.auth import Auth
-from api.v1.auth.basic_auth import BasicAuth
 
 
 app = Flask(__name__)
@@ -20,8 +19,6 @@ auth_type = getenv("AUTH_TYPE")
 
 if auth_type == "auth":
     auth = Auth()
-if auth_type == "basic_auth":
-    auth = BasicAuth()
 
 
 @app.errorhandler(404)
