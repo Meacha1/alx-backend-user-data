da38070ec770c4729fe50c4e17d52d5dfb70f95b
Meacha1 <meachattd@gmail.com>
2023-08-10 11:27:01 +0300

    401

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -26,8 +26,8 @@ def stats() -> str:
     return jsonify(stats)
 
 
-@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
-def unauthorized() -> str:
+@app_views.route('/unauthorized/', methods=['GET'], strict_slashes=False)
+def unauthorized() -> None:
     """ GET /api/v1/unauthorized
     Return:
       - unauthorized
